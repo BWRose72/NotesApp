395f7a7d13dc8e5d01ea1cc6b182cc41626a4b4e
IvailoW <Deskowaa@gmail.com>
2025-04-14 08:59:28 +0300

    LoadNotes-doesnt work/ create note maybe work

--- NotesApp/Form1.cs
+++ NotesApp/Form1.cs
@@ -3,38 +3,48 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Windows.Forms;
 using System.Data.SqlClient;
+using BusinessLogicLayer;
+using Microsoft.VisualBasic;
+using System.Collections;
 namespace NotesApp
 {
     public partial class Form1 : Form
     {
+        private readonly NotesServices noteServices;
+        private readonly TagsServices tagsServices;
+
         public Form1()
         {
             InitializeComponent();
+            noteServices = new NotesServices();
+            tagsServices = new TagsServices();
+            LoadNotes();
         }
         private void LoadNotes()
         {
-
-            List<String> notes = new List<String>();//За извеждане на бележки
+            
+            var notes = noteServices.GetNotesTitlesAndIDs();
             foreach (var note in notes)
             {
-                list_NotesPG1.Items.Add(note);
-                list_NotesPG4.Items.Add(note);
-                list_NotesPG4.Items.Add(note);
-                list_NotesPG6.Items.Add(note);
+                list_NotesPG1.Items.Add(note.Item2);
+                list_NotesPG4.Items.Add(note.Item2);
+                list_NotesPG4.Items.Add(note.Item2);
+                list_NotesPG6.Items.Add(note.Item2);
             }
         }
-        private void LoadTags()
+       /* private void LoadTags()
         {
-            List<String> tags = new List<String>();//за извеждане на тагове
+            List<> tags = tagsServices.GetNoteTags();
             foreach (var tag in tags)
             {
                 list_TagsPG3.Items.Add(tags);
                 list_TagsPG4.Items.Add(tags);
             }
-        }
+        }*/
         private void btn_DeleteNote_Click(object sender, EventArgs e)
         {
             string note = list_NotesPG1.SelectedItem.ToString();
+           
         }
 
         private void btn_ExportNote_Click(object sender, EventArgs e)
@@ -46,13 +56,14 @@ namespace NotesApp
         {
             string noteName = txt_NoteName.Text.Trim();
             string noteDescription = rich_NoteDescriptionPG2.Text;
+            noteServices.CreateNote(noteName, noteDescription);
             
         }
 
         private void btn_CreateTag_Click(object sender, EventArgs e)
         {
             string tagName = txt_TagNamePG3.Text.Trim();
-
+            tagsServices.CreateTag(tagName);
         }
 
         private void btn_AddTagToNote_Click(object sender, EventArgs e)
@@ -63,10 +74,13 @@ namespace NotesApp
         private void btn_UpdateNote_Click(object sender, EventArgs e)
         {
             string noteDescription = rich_NoteDescriptionPG5.Text;
+           
         }
 
         private void btn_SearcNotes_Click(object sender, EventArgs e)
         {
+            string tagName = txt_TagNamePG6.Text;
+            string noteDescription = rich_NoteDescriptionPG6.Text;
 
         }
 
