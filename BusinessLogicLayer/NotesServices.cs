999bf1fc5b4c97b3288894c4057d9facf7bda9a0
BWRose72 <sonq.ovanesian@gmail.com>
2025-06-10 13:53:52 +0300

    Added VanillaIris's summaries

--- BusinessLogicLayer/NotesServices.cs
+++ BusinessLogicLayer/NotesServices.cs
@@ -11,36 +11,69 @@ namespace BusinessLogicLayer
             notesDAL = new NotesDAL();
         }
 
+        /// <summary>
+        /// This method is used to get a list of the titles of the notes.
+        /// </summary>
         public List<(int, string)> GetNotesTitlesAndIDs()
         {
             return notesDAL.GetNotesTitlesAndIDs();
         }
 
+        /// <summary>
+        /// This method is used to get note content.
+        /// </summary>
+        /// <param name="noteID">The ID of the note.</param>
+        /// <returns>Returns note content.</returns>
         public string GetNoteContents(int noteID)
         {
             return notesDAL.GetNoteContent(noteID);
         }
 
+        /// <summary>
+        /// This method is used to create note.
+        /// </summary>
+        /// <param name="noteTitle">The title of the note.</param>
+        /// <param name="noteContent">The content of the note.</param>
+        /// <returns>true if it has successfully created the note, otherwise false.</returns>
         public bool CreateNote(string noteTitle, string noteContent)
         {
             return notesDAL.CreateNote(noteTitle, noteContent);
         }
 
+        /// <summary>
+        /// This method is used to update the notes.
+        /// </summary>
+        /// <param name="noteID">The ID of the note.</param>
+        /// <param name="noteContent">The content of the note.</param>
+        /// <returns>true if it has successfully updated the note, otherwise false.</returns>
         public bool UpdateNoteContents(int noteID, string noteContent)
         {
             return notesDAL.UpdateNoteContent(noteID, noteContent);
         }
 
+        /// <summary>
+        /// This method is used to delete notes.
+        /// </summary>
+        /// <param name="noteID">The ID of the note.</param>
+        /// <returns>true if it has successfully deleted the note, otherwise false.</returns>
         public bool DeleteNote(int noteID)
         {
             return notesDAL.DeleteNote(noteID);
         }
 
+        /// <summary>
+        /// This method is used to get a list of filtered notes by tag.
+        /// </summary>
+        /// <param name="tag">The name of the tag.</param>
         public List<(int, string)> GetFilteredNotes(string tag)
         {
             return notesDAL.GetFilteredNotes(tag.ToLower());
         }
 
+        /// <summary>
+        /// This method is used to get a list of notes by content.
+        /// </summary>
+        /// <param name="content">The content of the note.</param>
         public List<(int, string)> GetNotesByContent(string content)
         {
             return notesDAL.GetNotesByContent(content.ToLower());
--- BusinessLogicLayer/TagsServices.cs
+++ BusinessLogicLayer/TagsServices.cs
@@ -17,11 +17,18 @@ namespace BusinessLogicLayer
             tagsDAL = new TagsDAL();
         }
 
+        /// <summary>
+        /// This method is used to get a list of all tags.
+        /// </summary>
         public List<string> GetAllTags()
         {
             return tagsDAL.GetAllTags();
         }
 
+        /// <summary>
+        /// This method is used to get a list of tags that have not been added to notes.
+        /// </summary>
+        /// <param name="noteId">The ID of the note</param>
         public List<string> GetFreeTagsById(int noteId)
         {
             List<string> allTags = GetAllTags();
@@ -36,26 +43,51 @@ namespace BusinessLogicLayer
             return allTags;
         }
 
+        /// <summary>
+        /// This method is used to get a list of note tags.
+        /// </summary>
+        /// <param name="noteID">The ID of the note.</param>
         public List<string> GetNoteTags(int noteID)
         {
             return tagsDAL.GetNoteTags(noteID);
         }
 
+        /// <summary>
+        /// This method is used to create tags.
+        /// </summary>
+        /// <param name="tagContent">The name of the tag.</param>
+        /// <returns>If it's successful, returns the created tag.</returns>
         public bool CreateTag(string tagContent)
         {
             return tagsDAL.CreateTag(tagContent.ToLower());
         }
 
+        /// <summary>
+        /// This method is used to add tags to notes.
+        /// </summary>
+        /// <param name="noteID">The ID of the note.</param>
+        /// <param name="tagID">The ID of the tag.</param>
+        /// <returns>true if it has successfully added the tag to the note, otherwise false.</returns>
         public bool AddTagToNote(int noteID, int tagID)
         {
             return tagsDAL.AddTagToNote(noteID, tagID);
         }
 
+        /// <summary>
+        /// This method is used to delete tags.
+        /// </summary>
+        /// <param name="tagID">The ID of the tag.</param>
+        /// <returns>true if it has successfully deleted the tag, otherwise false.</returns>
         public bool DeleteTag(int tagID)
         {
             return tagsDAL.DeleteTag(tagID);
         }
 
+        /// <summary>
+        /// This method is used to get tag ID from the content.
+        /// </summary>
+        /// <param name="tagContent">The name of the tag.</param>
+        /// <returns>Returns the tag ID</returns>
         public int GetTagIDFromContent(string tagContent)
         {
             return tagsDAL.GetTagIDFromContent(tagContent.ToLower());
